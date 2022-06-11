54cb6d643bf4f56b9b33644f260ec56dc24a1384
CadijaTraore <98784824+CadijaTraore@users.noreply.github.com>
2022-06-11 16:53:10 +0100

    css

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let now = new Date
+let now = new Date();
 let h3 = document.querySelector("h3");
 
 let date = now.getDate();
@@ -6,10 +6,31 @@ let hours = now.getHours();
 let minutes = now.getMinutes();
 let year = now.getFullYear();
 
-let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 let day = days[now.getDay()];
 
-let months = ["January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December"];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let month = months[now.getMonth()];
 
-h3.innerHTML =`${day}, ${date} of ${month}. ${hours}:${minutes}`;
+h3.innerHTML = `${day}, ${date} of ${month}. ${hours}:${minutes}`;
