5c6d0253086ddbce184a1a0bdc958d50e1f492e4
CadijaTraore <98784824+CadijaTraore@users.noreply.github.com>
2022-06-11 14:09:10 +0100

    JS dates content added

---
+++ js.js
@@ -0,0 +1,15 @@
+let now = new Date
+let h3 = document.querySelector("h3");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+let day = days[now.getDay()];
+
+let months = ["January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December"];
+let month = months[now.getMonth()];
+
+h3.innerHTML =`${day}, ${date} of ${month}. ${hours}:${minutes}`;
